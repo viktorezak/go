a899d603b27b913462252a4e9f1510b8c4aa7aae
viktorezak <viktorezak@yandex.ru>
2023-11-20 17:05:33 +0300

    variables

---
+++ lesson1/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string
+	hello = "Hello go"
+	var symvol int32 = 'c'
+	var a int = 2023
+	b := 101
+	fmt.Println(hello)
+	fmt.Println(symvol)
+	fmt.Println(a, b)
+}
